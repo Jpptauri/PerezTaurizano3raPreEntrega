4149c22e4486970b2272aecc68f251b516f69103
Jpptauri <paulino@taurizano.com>
2024-11-01 17:58:44 -0300

    se agrego registro de usuarios y permisos de edicion

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import Template, Context, loader
 from home.forms import CrearMascotaFormulario,BuscarMascotaFormulario,EditarMascotaFormulario
 from home.models import Mascota
-
+from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request,'home/index.html')
 
@@ -20,6 +20,7 @@ def buscar_mascotas(request):
         
     return render(request, 'home/buscar_mascotas.html',{'mascotas':mascotas,'form':formulario})
 
+@login_required
 def crear_mascotas(request):
     formulario = CrearMascotaFormulario()
     if request.method == 'POST':
@@ -32,15 +33,16 @@ def crear_mascotas(request):
         
     return render(request,'home/crear_mascotas.html',{'form':formulario})
 
+@login_required
 def ver_mascota(request,id):
     mascota = Mascota.objects.get(id=id)
     return render(request,'home/ver_mascota.html',{'mascota':mascota})
-
+@login_required
 def eliminar_mascota(request,id):
     mascota = Mascota.objects.get(id=id)
     mascota.delete()
     return redirect('home:buscar_mascotas')
-
+@login_required
 def editar_mascota(request,id):
     mascota = Mascota.objects.get(id=id)
     formulario = EditarMascotaFormulario(initial={'nombre':mascota.nombre,'edad':mascota.edad,'especie':mascota.especie})
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login as django_login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as django_login
+from usuarios.forms import FormularioCreacionUsuarios
 
 def login(request):
-    
     formulario = AuthenticationForm()
-    
     if request.method == 'POST':
         formulario = AuthenticationForm(request, data = request.POST)
         if formulario.is_valid():
-            username_in = formulario.cleaned_data.get('username')
-            password_in = formulario.cleaned_data.get('password')
-            credentials = authenticate(username=username_in,password=password_in)
-            django_login(request,credentials)
+            django_login(request,formulario.get_user())
             return redirect('home:home')
-            
     return render(request,'usuarios/login.html',{'form':formulario})
+
+def register(request):
+    formulario = FormularioCreacionUsuarios()
+    if request.method == 'POST':
+        formulario = FormularioCreacionUsuarios(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('usuarios:login')
+    return render(request,'usuarios/register.html',{'form':formulario})
