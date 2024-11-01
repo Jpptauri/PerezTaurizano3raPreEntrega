09288fdafda7044ab8216d79decf225946269f39
Jpptauri <paulino@taurizano.com>
2024-11-01 19:32:47 -0300

    agrego funcion para cambiar de password

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,14 +1,28 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 
 class FormularioCreacionUsuarios(UserCreationForm):
     username = forms.CharField(label = 'Usuario')
     email = forms.EmailField(label = 'Email')
+    first_name = forms.CharField(label='Nombre')
+    last_name = forms.CharField(label='Apellido')
     password1 = forms.CharField(label = 'Contraseña',widget=forms.PasswordInput)
     password2 = forms.CharField(label = 'Repetir Contraseña',widget=forms.PasswordInput)
+
     
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['username','email','password1','password2','first_name','last_name']
         help_texts = {key: '' for key in fields}
+        
+class FormularioEdicionPerfil(UserChangeForm):
+    email = forms.EmailField(label='Email')
+    first_name = forms.CharField(label='Nombre')
+    last_name = forms.CharField(label='Apellido')
+    password = None
+    
+    class Meta:
+        model = User
+        fields = ['email','first_name','last_name']
+    
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as django_login
-from usuarios.forms import FormularioCreacionUsuarios
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from usuarios.forms import FormularioCreacionUsuarios,FormularioEdicionPerfil
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
 
 def login(request):
     formulario = AuthenticationForm()
@@ -20,3 +24,19 @@ def register(request):
             formulario.save()
             return redirect('usuarios:login')
     return render(request,'usuarios/register.html',{'form':formulario})
+@login_required
+def editar_perfil(request):
+    formulario = FormularioEdicionPerfil(instance=request.user)
+    
+    if request.method == 'POST':
+        formulario = FormularioEdicionPerfil(request.POST,instance=request.user)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('home:home')
+        
+    return render(request,'usuarios/editar_perfil.html',{'form':formulario})
+
+
+class CambiarPassword(LoginRequiredMixin,PasswordChangeView):
+    template_name = 'usuarios/cambiar_password.html'
+    success_url = reverse_lazy('usuarios:editar_perfil')
